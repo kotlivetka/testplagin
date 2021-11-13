5eb4ec66a540423cdf66afb58d5afe57ee02e366
cmit4 <milana.loginova2000@gmail.com>
2021-11-13 17:32:36 +0300

    t

--- src/main/kotlin/com/github/kotlivetka/testplugin/TestPlugin.kt
+++ src/main/kotlin/com/github/kotlivetka/testplugin/TestPlugin.kt
@@ -3,9 +3,14 @@ package com.github.kotlivetka.testplugin
 import com.destroystokyo.paper.event.player.PlayerJumpEvent
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
+import org.bukkit.DyeColor
 import org.bukkit.Material
+import org.bukkit.entity.EntityType
+import org.bukkit.entity.Sheep
+import org.bukkit.entity.Zombie
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
@@ -27,4 +32,17 @@ class TestPlugin : JavaPlugin(), Listener {
     fun onJump(event: PlayerJumpEvent) {
         event.player.inventory.addItem(ItemStack(Material.DIAMOND))
     }
+    @EventHandler
+    fun onObject(event: BlockBreakEvent) {
+       val entity= event.player.world.spawnEntity(event.block.location,EntityType.ZOMBIE)
+
+        entity as Zombie
+        entity.equipment.boots=ItemStack(Material.GOLDEN_BOOTS)
+        entity.equipment.leggings=ItemStack(Material.GOLDEN_LEGGINGS)
+        entity.equipment.chestplate=ItemStack(Material.GOLDEN_CHESTPLATE)
+        entity.equipment.helmet=ItemStack(Material.GOLDEN_HELMET)
+        entity.customName(Component.text("seva", NamedTextColor.DARK_PURPLE))
+        entity.isCustomNameVisible=true
+    }
+
 }
